34b746f8f8a6906dc544afb9b990400c0a8bfe43
Nasreen Mohsin <33446075+nasreenpmohsin@users.noreply.github.com>
2021-09-17 15:11:40 -0700

    Edited PoseModule.py

--- scripts/PoseModule.py
+++ scripts/PoseModule.py
@@ -23,8 +23,9 @@ class poseDetector():
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
-                                     self.detectionCon, self.trackCon)
+        #self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+        #                             self.detectionCon, self.trackCon)
+        self.pose   = self.mpPose.Pose(static_image_mode = self.mode,smooth_landmarks = self.smooth,min_detection_confidence = self.detectionCon, min_tracking_confidence = self.trackCon)
 
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -76,29 +77,4 @@ class poseDetector():
                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
 
-def main():
-    cap = cv2.VideoCapture('curls.mp4')
-    pTime = 0
-    detector = poseDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img, draw=False)
-        if len(lmList) != 0:
-            print(lmList[14])
-            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 0), 3)
-
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
 
